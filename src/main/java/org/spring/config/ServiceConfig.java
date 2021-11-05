7ca4a91b14047202ae21afef98ee098c31b5d7d6
Shubham Pant <shubhampant.mic@gmail.com>
2021-11-05 18:23:59 +0530

    adding messageDispatcherServlet for SOAP ws

---
+++ src/main/java/org/spring/config/ServiceConfig.java
@@ -0,0 +1,21 @@
+package org.spring.config;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+
+@EnableWs
+@Configuration
+public class ServiceConfig {
+	
+	@Bean
+	ServletRegistrationBean messageDispatcherServlet(ApplicationContext context) {
+		MessageDispatcherServlet messageDispatcherServlet = new MessageDispatcherServlet();
+		messageDispatcherServlet.setApplicationContext(context);
+		messageDispatcherServlet.setTransformWsdlLocations(true);
+		return new ServletRegistrationBean(messageDispatcherServlet,"/ws/*");
+	}
+}
